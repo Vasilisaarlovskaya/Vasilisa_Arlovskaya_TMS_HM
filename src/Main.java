451829f32996bca64b1c527936078fd4c1d1292f
Vasilisa Arlovskaya <vasilisaarlovskaya@gmail.com>
2022-11-05 13:57:36 +0300

    without mistakes

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
+
         Scanner scanner = new Scanner(System.in);
+        int group =0;
 
         System.out.println("Введите число, пожалуйста");
         int number = scanner.nextInt();
         if (0 < number && number < 15) {
-            i = 1;
-            System.out.println("Группа " + i);
-        } else
-        if (number >= 15 & number < 30) {
-            i = 2;
-            System.out.println("Группа " + i);
-        } else
-        if (number >= 30) {
-            i = 3;
-            System.out.println("Группа " + i);
+            System.out.println("Группа 1");
+            group = 1;
+        } else if (number >= 15 && number < 30) {
+            System.out.println("Группа 2");
+            group = 2;
+        } else if (number >= 30) {
+            System.out.println("Группа 3");
+            group = 3;
         } else
             System.out.println("НЕ В ДИАПОЗОНЕ, проверьте число");
 
-        if (i == 1) {
+        for (int i = 0; i < group; i++) {
             System.out.println("Дорогая, хочешь убить всех детей? (Бендер)");
-        } if (i == 2) {
-            System.out.println("Пиво рулит, не ты!");
-            System.out.println("Хочешь я немного поглумлюсь над трупом? (Бендер)");
-        } if (i == 3) {
-            System.out.println("Смотреть порно и зарабатывать?! Что-то мне не верится.");
-            System.out.println("Я не так знаменит, чтобы давить людей безнаказанно. ");
-            System.out.println("Не бейте меня!... я предам кого угодно. (Бендер)");
         }
     }
 }
