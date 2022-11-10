28bbebedf649b6a8660c876bcdc8554159585eaa
Vasilisa Arlovskaya <vasilisaarlovskaya@gmail.com>
2022-11-10 22:20:56 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static  void main(String [] args) {
+        Cat barsik = new Cat("barsik", "wit", 3, 4);
+        Cat murzik = new Cat( "murzik", "Wit");
+        Cat mraz = new Cat ("mraz", "Wit",1,  Cat.eat());
+        System.out.println(barsik.name);
+
+        barsik.age = 2;
+        System.out.println(barsik.age);
+
+
+        /* barsik.voice(); */
+        barsik. weight += Cat.eat();
+        System.out.println("Барсик весит = " + barsik.weight);
+        Cat.method(barsik, murzik, mraz);
+
+        murzik.age = 3;
+        System.out.println(murzik.toString("GAV"));
+
+        mraz.balls = true;
+        mraz.castrasia (mraz.castrasia(mraz.balls));
+
     }
 }
