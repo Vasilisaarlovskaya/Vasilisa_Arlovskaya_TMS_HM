0f4bea0559621a535b96b1116ae6aa020d90151c
Vasilisa Arlovskaya <vasilisaarlovskaya@gmail.com>
2022-11-20 00:03:14 +0300

    second exercise

---
+++ src/Lesson8_2/Dentist.java
@@ -0,0 +1,9 @@
+package Lesson8_2;
+
+public class Dentist {
+    public static void treat (int teeth) {
+        System.out.println("Дантист лечит");
+        teeth = 32;
+        System.out.println("После лечения пациента у дантиста зубов в полости рта: " + teeth);
+    }
+}
---
+++ src/Lesson8_2/Therapist.java
@@ -0,0 +1,12 @@
+package Lesson8_2;
+
+public class Therapist {
+    public static void treat (int diseas){
+
+        System.out.println("Терапевт лечит.");
+        diseas = 0;
+        System.out.println("Полечил, полечил - вылечил. Болезней: " + diseas);
+
+    }
+
+}
