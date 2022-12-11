338cf1e376d9dc2418666125f9e468a157a2053a
Vasilisa Arlovskaya <vasilisaarlovskaya@gmail.com>
2022-12-12 02:21:09 +0300

    lesson10

--- src/lesson10_2/Main.java
+++ src/lesson10_2/Main.java
@@ -7,6 +7,7 @@
 package lesson10_2;
 
 public class Main {
+
     public static void main(String[] args) {
         StringBuilder outrage = new StringBuilder("MyBad");
         StringBuilder doubleWorld= new StringBuilder();
