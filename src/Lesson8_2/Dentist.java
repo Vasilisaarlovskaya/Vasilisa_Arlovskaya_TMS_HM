96622ca5aa88cb3bdec0a44f682e21874fd2c083
Vasilisaarlovskaya <116302303+Vasilisaarlovskaya@users.noreply.github.com>
2023-02-11 02:07:30 +0300

    Delete Dentist.java

--- src/Lesson8_2/Dentist.java
+++
@@ -1,9 +0,0 @@
-package Lesson8_2;
-
-public class Dentist {
-    public static void treat (int teeth) {
-        System.out.println("Дантист лечит");
-        teeth = 32;
-        System.out.println("После лечения пациента у дантиста зубов в полости рта: " + teeth);
-    }
-}
