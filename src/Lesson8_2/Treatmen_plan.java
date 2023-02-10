bfcc6e8d027f541bf4f2b2a2f8786d4d36ba1fbf
Vasilisaarlovskaya <116302303+Vasilisaarlovskaya@users.noreply.github.com>
2023-02-11 02:08:08 +0300

    Delete Treatmen_plan.java

--- src/Lesson8_2/Treatmen_plan.java
+++
@@ -1,8 +0,0 @@
-package Lesson8_2;
-
-public class Treatmen_plan {
-    Treatmen_plan plan1 = new Treatmen_plan();
-    Treatmen_plan plan2 = new Treatmen_plan();
-    Treatmen_plan plan3 = new Treatmen_plan();
-
-}
