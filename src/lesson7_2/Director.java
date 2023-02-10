160131c8d79a0e4c15163374b2646088dc33cd3f
Vasilisaarlovskaya <116302303+Vasilisaarlovskaya@users.noreply.github.com>
2023-02-11 02:10:43 +0300

    Delete Director.java

--- src/lesson7_2/Director.java
+++
@@ -1,9 +0,0 @@
-package lesson7_2;
-
-public class Director implements Printable {
-    String name = "Директор";
-    @Override
-    public void print() {
-        System.out.println(name);
-    }
-}
