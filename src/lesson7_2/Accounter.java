ef1af048f53b6ba60024636c3969c43666609128
Vasilisaarlovskaya <116302303+Vasilisaarlovskaya@users.noreply.github.com>
2023-02-11 02:10:36 +0300

    Delete Accounter.java

--- src/lesson7_2/Accounter.java
+++
@@ -1,9 +0,0 @@
-package lesson7_2;
-
-public class Accounter implements Printable {
-    String name = "Бухгалтер";
-    @Override
-    public void print() {
-        System.out.println(name);
-    }
-}
