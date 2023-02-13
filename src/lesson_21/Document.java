4a23c1557be208c0e5c4709899b1fc9f48ec5fa3
Vasilisa Arlovskaya <vasilisaarlovskaya@gmail.com>
2023-02-13 18:25:58 +0300

    Lesson 21

--- src/lesson_21/Document.java
+++ src/lesson_21/Document.java
@@ -5,19 +5,28 @@ import java.text.SimpleDateFormat;
 public class Document {
 
     private String number;
+    private String inf;
+    private String typeDocument;
 
     public String getNumber() {
         return number;
     }
+    public String getInf () {return inf;}
 
+    public String getTypeDocument() {return typeDocument;}
 
     public void setNumber(String number) {
         this.number = number;
     }
+    public void setInf (String inf) {this.inf = inf;}
+
+    public void setTypeDocument(String typeDocument) {this.typeDocument = typeDocument;}
 
     SimpleDateFormat date = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
-    public Document(String number) {
+    public Document(String number,String typeDocument, String inf) {
         this.number = number;
+        this.inf = inf;
+        this.typeDocument = typeDocument;
         this.date = date;
     }
 }
